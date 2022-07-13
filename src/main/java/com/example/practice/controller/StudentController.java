cd535b3792d85b0b52fdce5258d6318679f10c06
Maxim Goy <maksgoy2911@gmail.com>
2022-07-13 21:19:47 +0300

    add controller StudentController

--- src/main/java/com/example/practice/controller/StudentController.java
+++ src/main/java/com/example/practice/controller/StudentController.java
@@ -1,15 +1,44 @@
 package com.example.practice.controller;
 
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.practice.model.Student;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Controller
 public class StudentController {
     @ResponseBody
     @GetMapping("/students")
-    public String getStudents(){
+    public String getStudents(@RequestParam(name="SID", defaultValue = "0", required = false) int SID, Model model){
+        String path="C:/Users/Asus/Desktop/ISiT/Spring-boot-application/src/main/resources/students.json";
+        String message="";
+        message+=SID+"\n";
+        String json;
+        Gson gson=new Gson();
+
+        try{
+            json = new String(Files.readAllBytes(Paths.get(path)));
+        }
+        catch (Exception e){
+            return "Error read file";
+        }
+        ArrayList<Student> students=gson.fromJson(json, new TypeToken<ArrayList<Student>>(){}.getType());
         
-        return "student Victor";
+        for(Student student : students){
+            if(student.id % 10 == SID)
+                message += student;
+            //if(name=)
+        }
+        return message;
     }
 }
---
+++ src/main/java/com/example/practice/model/Student.java
@@ -0,0 +1,36 @@
+package com.example.practice.model;
+
+public class Student {
+    public static int count = 1;
+    public int id;
+    public String name;
+    public String lastname;
+    public String patronic;
+    public int course;
+    public String faculty;
+
+    private Student(String name, String lastname, String patronic, int course, String faculty) {
+        this.id = count;
+        this.count++;
+        this.name = name;
+        this.lastname = lastname;
+        this.patronic = patronic;
+        this.course = course;
+        this.faculty = faculty;
+    }
+
+    public static Student createStudent(String name, String lastname, String patronic, int course, String faculty) {
+        return new Student(name, lastname, patronic, course, faculty);
+    }
+
+    @Override
+    public String toString() {
+        return "\nid: " + id +
+                "\nname: " + name +
+                "\nlastname: " + lastname +
+                "\npatronic: " + patronic +
+                "\ncourse: " + course +
+                "\nfaculty: " + faculty;
+    }
+
+}
