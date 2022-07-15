a85cface6f821b806d24b31e204ad7b14179202b
Maxim Goy <maksgoy2911@gmail.com>
2022-07-15 09:33:49 +0300

    we take list on based on SID

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM ubuntu
 FROM java:8
 
 COPY target/practice-0.0.1-SNAPSHOT.jar /demo.jar
+COPY students.json /students.json
 
 CMD ["java", "-jar", "/demo.jar"]
--- src/main/java/com/example/practice/controller/StudentController.java
+++ src/main/java/com/example/practice/controller/StudentController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,14 +18,22 @@ import com.google.gson.reflect.TypeToken;
 
 @Controller
 public class StudentController {
-    String path="C:/Users/Asus/Desktop/ISiT/Spring-boot-application/src/main/resources/students.json";
+    String path = "students.json";
+    ArrayList<Student> students = new ArrayList<Student>();
+    int SID;
+
     @ResponseBody
-    @GetMapping("/students")
-    public String Students(@RequestParam(name="SID", defaultValue = "-1", required = false) int SID,
+    @GetMapping("/")
+    public String Students(/*@RequestHeader(value = "Accept", defaultValue = "") String accept,*/
+        @RequestParam(name="SID",  required = true) int SID,
          @RequestParam(name="faculty", defaultValue = "", required = false) String faculty,
-         @RequestParam(name="ID", defaultValue = "-1", required = false) int Id,
+         @RequestParam(name="id", defaultValue = "-1", required = false) int Id,
          Model model){
-        
+        /*if(accept.compareTo("") == 0)
+            return "Error";*/
+
+        this.students=getStudents(SID);
+
         String message="<!DOCTYPE html>" +
         "<html lang=\"en\">"+
         "<head>"+
@@ -33,19 +42,13 @@ public class StudentController {
         "</head>"+
         "<body>" +
         "<p>";
-        ArrayList<Student> students = new ArrayList<Student>();
-        if(SID != -1)
-            students = getStudentsOnSID(SID);
-
-        else if(Id != -1){
-            students = getStudentsOnId(Id);
+        if(Id != -1){
+            this.students = getStudentsOnId(Id);
         }
         else if(!faculty.equals(""))
-            students = getStudentsOnFaculty(faculty);
-        else
-            return "Error";
+            this.students = getStudentsOnFaculty(faculty);
 
-        for(Student student: students){
+        for(Student student: this.students){
             message += student + "</p><p>";
         }
         message += "</p></body></html>";
@@ -53,38 +56,26 @@ public class StudentController {
     }
 
     public ArrayList<Student> getStudentsOnFaculty(String faculty){
-        ArrayList<Student> students = getStudents();
-        ArrayList<Student> st = new ArrayList<Student>();
+        ArrayList<Student> temp=new ArrayList<Student>();
         for(Student student: students){
             if(student.faculty.equals(faculty))
-                st.add(student);
+                temp.add(student);
         }
-        return st;
+        return temp;
     }
 
     public ArrayList<Student> getStudentsOnId(int Id){
-        ArrayList<Student> students = getStudents();
-        ArrayList<Student> st = new ArrayList<Student>();
-        for(Student student: students){
+        ArrayList<Student> temp=new ArrayList<Student>();
+        for(Student student: this.students){
             if(student.id == Id){
-                st.add(student);
+                temp.add(student);
                 break;
             }
         }
-        return st;
+        return temp;
     }
 
-    public ArrayList<Student> getStudentsOnSID(int SID){
-        ArrayList<Student> students = getStudents();
-        ArrayList<Student> st = new ArrayList<Student>();
-        for(Student student: students){
-            if(student.id % 10 == SID)
-                st.add(student);
-        }
-        return st;
-    }
-
-    public ArrayList<Student> getStudents(){
+    public ArrayList<Student> getStudents(int SID){
         Gson gson = new Gson();
         String json = "";
 
@@ -96,6 +87,15 @@ public class StudentController {
         }
 
         ArrayList<Student> students = gson.fromJson(json, new TypeToken<ArrayList<Student>>(){}.getType());
-        return students;
+        ArrayList<Student> temp = new ArrayList<Student>();
+        int id = 0;
+        for(Student student: students){
+            if(student.id % 10 == SID){
+                student.id = id;
+                temp.add(student);
+                id++;
+            }
+        }
+        return temp;
     }
 }
